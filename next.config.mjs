ed6288d56b71ea9aeb5e568a21d4ea19a9ba35c7
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-04 01:11:10 +0300

    refactor: use standalone output if not on vercel
    Make sure production build works on both Vercel & Docker image.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: 'utfs.io', protocol: 'https' }] },
+  output: process.env.VERCEL ? undefined : 'standalone',
 };
 
 export default nextConfig;
