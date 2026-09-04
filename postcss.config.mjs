8f59c2058f14ec3a300d39b45128e55447172758
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-04 14:25:44 +0300

    chore(deps): update tailwind & related deps

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-mixins': {},
-    tailwindcss: {},
-    'postcss-hover-media-feature': {},
-    'postcss-viewport-unit-fallback': {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
 
