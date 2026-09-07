870a081943f2f4aa63b7e96607f59562f0f8ff64
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-07 23:06:13 +0300

    fix(stream): interaction needed UI not appearing

--- src/components/layout/StreamPlayer/StreamLiveVideo.tsx
+++ src/components/layout/StreamPlayer/StreamLiveVideo.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import { useCallback, useEffect, useRef, useState } from 'react';
-import {
-  useMaybeRoomContext,
-  useTracks,
-} from '@livekit/components-react';
-import { RoomEvent, Track } from 'livekit-client';
+import { useAudioPlayback, useTracks } from '@livekit/components-react';
+import { Track } from 'livekit-client';
 import { useEventListener } from 'usehooks-ts';
 
 import { useStream } from '@/hooks';
@@ -18,24 +15,11 @@ import { StreamVolumeControl } from './StreamVolumeControl';
 export const StreamLiveVideo = () => {
   const [volume, setVolume] = useState(100),
     [muted, setMuted] = useState(false),
-    [isFullscreen, setIsFullscreen] = useState(false),
-    [interactionNeeded, setInteractionNeeded] = useState(false);
+    [isFullscreen, setIsFullscreen] = useState(false);
 
   const { hostId } = useStream();
-  const room = useMaybeRoomContext(),
+  const { canPlayAudio } = useAudioPlayback(),
     tracks = useTracks([Track.Source.Camera, Track.Source.Microphone]);
-
-  useEffect(() => {
-    if (!room) return;
-
-    /* By default browsers block audio autoplay until the user interacts
-    with the page. In this case display a button to manually play audio. */
-    const listener = () => setInteractionNeeded(!room.canPlaybackAudio);
-    room.on(RoomEvent.AudioPlaybackStatusChanged, listener);
-    return () => {
-      room.off(RoomEvent.AudioPlaybackStatusChanged, listener);
-    };
-  }, [room]);
 
   const wrapperRef = useRef<HTMLDivElement>(null),
     videoRef = useRef<HTMLVideoElement>(null);
@@ -89,11 +73,11 @@ export const StreamLiveVideo = () => {
 
   return (
     <div ref={wrapperRef} className='group relative flex h-full'>
-      {interactionNeeded && <StreamInteractionNeeded />}
+      {!canPlayAudio && <StreamInteractionNeeded />}
       <video
         ref={videoRef}
         width='100%'
-        className={cn('transition-[filter]', interactionNeeded && 'blur-lg')}
+        className={cn('transition-[filter]', !canPlayAudio && 'blur-lg')}
       />
       <div className='absolute bottom-0 flex h-14 w-full items-center justify-between bg-linear-to-t from-black px-2 opacity-0 transition-opacity group-hover:opacity-100'>
         <StreamVolumeControl
