8ea655c8b6088e1d566aaa30667cccfbb39d4e9f
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-11 00:52:30 +0300

    chore(deps): remove `@radix-ui/react-slot`
    `Slot` can now be imported from `radix-ui` instead.

--- src/components/layout/Sidebar/SidebarListItemIcon.tsx
+++ src/components/layout/Sidebar/SidebarListItemIcon.tsx
@@ -1,12 +1,12 @@
 import type { ComponentPropsWithoutRef } from 'react';
-import { Slot } from '@radix-ui/react-slot';
 import { cn } from 'cn';
+import { Slot } from 'radix-ui';
 
 export const SidebarListItemIcon = ({
   className,
   children,
 }: ComponentPropsWithoutRef<'svg'>) => (
-  <Slot className={cn('mb-1 shrink-0 lg:me-4 lg:mb-0', className)}>
+  <Slot.Root className={cn('mb-1 shrink-0 lg:me-4 lg:mb-0', className)}>
     {children}
-  </Slot>
+  </Slot.Root>
 );
