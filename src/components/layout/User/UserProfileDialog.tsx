ab0996ddf11aa9d051ee5da3cc3504647bbc7014
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-11 17:25:22 +0300

    refactor(dialog): remove compatibility index

--- src/components/layout/User/UserProfileDialog.tsx
+++ src/components/layout/User/UserProfileDialog.tsx
@@ -17,7 +17,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from '@/components/ui/Dialog';
+} from '@/components/ui/dialog';
 import { FormField } from '@/components/ui/Form';
 import { SpinnerButton } from '@/components/ui/SpinnerButton';
 import { Textarea } from '@/components/ui/Textarea';
