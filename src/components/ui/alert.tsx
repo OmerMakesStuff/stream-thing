0999355a3fe93c9ce720c8937c79f96e321f0162
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-18 18:35:52 +0300

    refactor(ui): update alert bg & text colors

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -7,9 +7,9 @@ const alertVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-muted text-foreground',
+        default: 'bg-card text-card-foreground',
         destructive:
-          'bg-muted text-destructive *:data-[slot=alert-description]:text-destructive/90 *:[svg]:text-current',
+          'bg-card text-destructive *:data-[slot=alert-description]:text-destructive/90 *:[svg]:text-current',
       },
     },
     defaultVariants: { variant: 'default' },
