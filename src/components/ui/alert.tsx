a940508bcab7b5b3c17224ddb38216036186ac47
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-18 18:35:26 +0300

    refactor(ui): update alert with logical classes

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from 'cn';
 
 const alertVariants = cva(
-  "group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-18 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2.5 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
+  "group/alert relative grid w-full gap-0.5 rounded-lg border px-4 py-3 text-start text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pe-18 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2.5 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
@@ -57,7 +57,7 @@ export const AlertDescription = ({
 export const AlertAction = ({ className, ...props }: ComponentProps<'div'>) => (
   <div
     data-slot='alert-action'
-    className={cn('absolute top-2.5 right-3', className)}
+    className={cn('absolute inset-e-3 top-2.5', className)}
     {...props}
   />
 );
