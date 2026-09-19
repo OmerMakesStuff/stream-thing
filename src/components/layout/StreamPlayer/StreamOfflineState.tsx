8a744ab349340445438618492d2a4017593858d4
Omer <27678413+OmerMakesStuff@users.noreply.github.com>
2026-09-19 13:02:54 +0300

    refactor(stream-player): simplify classes

--- src/components/layout/StreamPlayer/StreamOfflineState.tsx
+++ src/components/layout/StreamPlayer/StreamOfflineState.tsx
@@ -4,7 +4,7 @@ export type StreamOfflineStateProps = { username?: string };
 
 export const StreamOfflineState = ({ username }: StreamOfflineStateProps) => (
   <div className='flex h-full flex-col items-center justify-center gap-4 text-white/55'>
-    <WifiOffIcon className='h-10 w-10' />
+    <WifiOffIcon className='size-10' />
     <p>{username ? `${username} is offline` : 'Offline'}</p>
   </div>
 );
